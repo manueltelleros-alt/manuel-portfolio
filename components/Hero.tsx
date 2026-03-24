2fe9bc217b07c60dfc6e1538d06afd7ec6ac1b80
manueltelleros-alt <manueltelleros@sksu.edu.ph>
2026-03-24 09:37:35 +0800

    awdad

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,7 +33,7 @@ export function Hero() {
                transform transition duration-700 ease-out hover:scale-105 hover:text-green-500">
             Buddy Custodio
           </h1>
-            <p className="mt-6 mb-4 text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-center sm:text-left tracking-wide font-open sans text-lg text-gray-700 flex justify-center">
+            <p className="mt-6 mb-4 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center sm:text-left tracking-wide flex justify-center bg-gradient-to-r from-green-500 via-emerald-500 to-teal-500 bg-clip-text text-transparent leading-relaxed">
             I am a BSIS student at Sultan Kudarat State University (SKSU) with a passion for building efficient solutions, automating processes, and developing impactful applications. I have gained strong technical and analytical skills through academic projects and hands-on experience, allowing me to adapt to new technologies and work effectively in team environments.
           </p>
   
